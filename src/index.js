2bc395e1b8e972f48bda06954084366642e51be3
Victoria Zubareva <lion.studio2013@yandex.ru>
2019-03-03 15:28:01 +0300

    solutions index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { rejects } from "assert";
+
 /* ДЗ 6 - Асинхронность и работа с сетью */
 
 /*
@@ -9,6 +11,9 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), seconds * 1000);
+    });
 }
 
 /*
@@ -25,6 +30,32 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 400) {
+                reject();
+            }
+
+            const sortTowns = xhr.response.sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1;
+                } else if (b.name > a.name) {
+                    return -1;
+                }
+
+                return 0;
+            });
+
+            return resolve(sortTowns);
+        })
+        xhr.addEventListener('error', reject);
+        xhr.addEventListener('about', reject);
+    });
 }
 
 export {
